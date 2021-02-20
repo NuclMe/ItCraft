d5c6abe20db008b57afdb82362e70061c7544e35
NuclMe <saltansergei@gmail.com>
2021-02-20 22:33:23 +0200

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,33 @@
-import React from 'react';
-import logo from '../logo.svg'; 
+import React from "react";
+import logo from "../logo.svg";
 
-function Header(){
-    return(
-        <header>
-            
-                <div className='header-inner'>
-                        <a href="#" className='logo-wrapper'>
-                            <img src={logo}alt=""/>
-                        </a>
-                            <menu className='menu'>
-                                 <li>
-                                     <a href="#" className="item">
-                                         Blog
-                                     </a>
-                                 </li>
-                                 <li>
-                                     <a href="#" className="item">
-                                         Contact
-                                     </a>
-                                 </li>
-                                 <li>
-                                     <a href="#" className="item">
-                                         Help
-                                     </a>
-                                 </li>
-                                 <li>
-                                     <a href="#" className="item">
-                                         Dealer
-                                     </a>
-                                 </li>
-                                 <li>
-                                     <a href="#" className="item">
-                                         Carrier
-                                     </a>
-                                 </li>
-                                 
-                            </menu>
-                </div>
+function Header() {
+  return (
+    <header>
+      <div className="header-inner">
+        <a href="#" className="logo-wrapper">
+          <img src={logo} alt="" />
+        </a>
+        <menu className="menu">
+          <li className="item">
+            <a href="#">Blog</a>
+          </li>
+          <li className="item">
+            <a href="#">Contact</a>
+          </li>
+          <li className="item">
+            <a href="#">Help</a>
+          </li>
+          <li className="item">
+            <a href="#">Dealer</a>
+          </li>
+          <li className="item">
+            <a href="#">Carrier</a>
+          </li>
+        </menu>
+      </div>
+    </header>
+  );
+}
 
-                
-
-            
-        </header>
-    );
-};
-
-export {Header};
+export { Header };
