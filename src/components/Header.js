d1347d8149486d07aae600c4b5565c179c81c72d
NuclMe <saltansergei@gmail.com>
2021-02-20 20:49:49 +0200

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
+import React from 'react';
+import logo from '../logo.svg'; 
+
 function Header(){
     return(
         <header>
-            <div className="container">
-                <img src="{}" alt=""/>
-            </div>
+            
+                <div className='header-inner'>
+                        <a href="#" className='logo-wrapper'>
+                            <img src={logo}alt=""/>
+                        </a>
+                            <menu className='menu'>
+                                 <li>
+                                     <a href="#" className="item">
+                                         Blog
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href="#" className="item">
+                                         Contact
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href="#" className="item">
+                                         Help
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href="#" className="item">
+                                         Dealer
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href="#" className="item">
+                                         Carrier
+                                     </a>
+                                 </li>
+                                 
+                            </menu>
+                </div>
+
+                
+
+            
         </header>
     );
 };
 
-export default Header;
+export {Header};
