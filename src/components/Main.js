2ee08d6ac3f32ab83e78e2ba76949fa4f01550b8
NuclMe <saltansergei@gmail.com>
2021-02-22 10:20:48 +0200

    super commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,30 +13,259 @@ function Main(){
                 </a>
                 <div className='main-inner'>
                     <aside>
-                       <div className='sorting-holder'>
-                           <div className="sorting-item">
-                                <div className="tabs-container">
-                                    <a href="#" className="tab">All</a>
-                                    <a href="#" className="tab active">Used</a>
-                                    <a href="#" className="tab">Classics</a>
-                                    <a href="#" className="tab">New</a>
+                      <div className='sorting-wrapper'>
+                           <div className='sorting-holder'>
+                               {/* <div className="sorting-item"> */}
+                                    <div className="tabs-container">
+                                        <a href="#" className="tab">All</a>
+                                        <a href="#" className="tab active">Used</a>
+                                        <a href="#" className="tab">Classics</a>
+                                        <a href="#" className="tab">New</a>
+                                    </div>
+                               {/* </div> */}
+                           </div>
+                           <div className='sorting-holder'>
+                               <div className="title">
+                                   Vehicile type
+                               </div>
+                               <div className='input-holder'>
+                                    <span className='line'></span>
+                                    <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                    <div name="1" id="car-type_list" className='dropdown-holder'>
+                                        <svg className="svg-icon">
+                                                <use xlinkHref="#icon-arrow-down"></use>
+                                        </svg>
+                                        <div className="dropdown-inner">
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>                                       
+                                        </div>
+                                    </div>
+                                </div>
+                           </div>
+                           <div className='sorting-holder'>                    
+                                <div className="title">
+                                        Manufacturer
+                                </div>
+                                <div className='input-holder'>
+                                <span className='line'></span>
+                                    <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                    <div name="1" id="car-type_list" className='dropdown-holder'>
+                                        <svg className="svg-icon">
+                                                <use xlinkHref="#icon-cross"></use>
+                                        </svg>
+                                        <div className="dropdown-inner">
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>                                       
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                           <div className='sorting-holder'>
+                               <div className="title">
+                                   Model
+                               </div>
+                               <div className='input-holder filtered'>
+                                    <span className='line'></span>
+                                    <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                    <div name="1" id="car-type_list" className='dropdown-holder'>
+                                        <svg className="svg-icon">
+                                                <use xlinkHref="#icon-arrow-down"></use>
+                                        </svg>
+                                        <div className="dropdown-inner">
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>
+                                            <span className='item'> Audi</span>                                       
+                                        </div>
+                                    </div>
                                 </div>
                            </div>
                        </div>
-                       <div className='sorting-holder'>
-                           <div className="title">
-                               Vehicile type
+                      <div className="sorting-wrapper">
+                            <div className='sorting-holder'>                            
+                                   <div className="title">
+                                       Model
+                                   </div>
+                                <div className='sorting-inner'>
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                        <div name="1" id="car-type_list" className='dropdown-holder'>
+                                            <svg className="svg-icon">
+                                                    <use xlinkHref="#icon-arrow-down"></use>
+                                            </svg>
+                                            <div className="dropdown-inner">
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>                                       
+                                            </div>
+                                        </div>
+                                    </div>
+                             
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                        <div name="1" id="car-type_list" className='dropdown-holder'>
+                                            <svg className="svg-icon">
+                                                    <use xlinkHref="#icon-arrow-down"></use>
+                                            </svg>
+                                            <div className="dropdown-inner">
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>                                       
+                                            </div>
+                                        </div>
+                                    </div>
+                               </div>
                            </div>
-                           <input type="select"/>
-                           <select name="1" id="car-type">
-                               <option value="1">
-                                   Toyota
-                               </option>
-                               <option value="2">
-                                   Subaru
-                               </option>
-                           </select>
+                            <div className='sorting-holder'>                            
+                                   <div className="title">
+                                        Milleage(miles)
+                                   </div>
+                                <div className='sorting-inner'>
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                        <div name="1" id="car-type_list" className='dropdown-holder'>
+                                            <svg className="svg-icon">
+                                                    <use xlinkHref="#icon-arrow-down"></use>
+                                            </svg>
+                                            <div className="dropdown-inner">
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>                                       
+                                            </div>
+                                        </div>
+                                    </div>
+                             
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                            
+                                        <div name="1" id="car-type_list" className='dropdown-holder'>
+                                            <svg className="svg-icon">
+                                                    <use xlinkHref="#icon-arrow-down"></use>
+                                            </svg>
+                                            <div className="dropdown-inner">
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>
+                                                <span className='item'> Audi</span>                                       
+                                            </div>
+                                        </div>
+                                    </div>
+                               </div>
+                           </div>
+                            <div className='sorting-holder'>                            
+                                   <div className="title">
+                                        Price($)
+                                   </div>
+                                <div className='sorting-inner'>
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                                                                    
+                                    </div>
+                             
+                                   <div className='input-holder filtered'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                                                                  
+                                    </div>
+                               </div>
+                           </div>
                        </div>
+                      <div className="sorting-wrapper">
+                        <div className="title">
+                            Transmission
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option1' className='checkbox'/>
+                            <label htmlFor="option1">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option2' className='checkbox'/>
+                            <label htmlFor="option2">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option3' className='checkbox'/>
+                            <label htmlFor="option3">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option4' className='checkbox'/>
+                            <label htmlFor="option4">
+                                Manual gearbox
+                            </label>
+                        </div>
+                      </div>
+                      <div className="sorting-wrapper">
+                            <div className='sorting-holder'>                            
+                                   <div className="title">
+                                     Days on Market
+                                   </div>
+                                <div className='sorting-inner'>
+                                   <div className='input-holder'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='From' id='car-type_input'/>                            
+                                       
+                                    </div>                             
+                                   <div className='input-holder filtered'>
+                                        <span className='line'></span>
+                                        <input type="select" className='text-field' placeholder='All' id='car-type_input'/>                                                                 
+                                    </div>
+                               </div>
+                           </div>  
+                        </div>
+                      <div className="sorting-wrapper">
+                        <div className="title">
+                          Fuel type
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option1' className='checkbox'/>
+                            <label htmlFor="option1">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option2' className='checkbox'/>
+                            <label htmlFor="option2">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option3' className='checkbox'/>
+                            <label htmlFor="option3">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option4' className='checkbox'/>
+                            <label htmlFor="option4">
+                                Manual gearbox
+                            </label>
+                        </div>
+                      </div>
+                      <div className="sorting-wrapper">
+                        <div className="title">
+                            New/Used / CPO
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option1' className='checkbox'/>
+                            <label htmlFor="option1">
+                                Manual gearbox
+                            </label>
+                        </div>
+                        <div className="row">
+                            <input type="checkbox" name='manual' id='option2' className='checkbox'/>
+                            <label htmlFor="option2">
+                                Manual gearbox
+                            </label>
+                        </div>
+                      
+                      </div>
                     </aside>
                     <section>        
                         21212        
